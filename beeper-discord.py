c84a78c25110f4a7fada9b92c0eed4c21c226e08
Cameron Aaron <cameronaaron1@gmail.com>
2024-05-03 04:01:48 -0700

    updates too

--- beeper-discord.py
+++ beeper-discord.py
@@ -7,6 +7,7 @@ TOKEN = 'your-discord-bot-token'
 CHANNEL_ID = 123456789
 API_URL_1 = 'https://beeperstatus.com/summary.json'
 API_URL_2 = 'https://beeperstatus.com/v2/components.json'
+API_URL_3 = 'https://updates.beeper.com/updates.json'
 
 bot = commands.Bot(command_prefix='!')
 
@@ -14,6 +15,7 @@ bot = commands.Bot(command_prefix='!')
 async def on_ready():
     print(f'We have logged in as {bot.user}')
     check_api.start()  # Start the task when the bot is ready
+    check_updates.start()  # Start the updates check when the bot is ready
 
 @tasks.loop(minutes=1)  # Check every 1 minute
 async def check_api():
@@ -30,5 +32,14 @@ async def check_api():
                 if component['status'] != 'OPERATIONAL':
                     channel = bot.get_channel(CHANNEL_ID)
                     await channel.send(f'Component {component["name"]} is not operational! Status: {component["status"]}')
+
+@tasks.loop(minutes=1)  # Check every 1 minute
+async def check_updates():
+    async with aiohttp.ClientSession() as session:
+        async with session.get(API_URL_3) as response3:
+            data3 = await response3.json()
+            for item in data3['items']:
+                channel = bot.get_channel(CHANNEL_ID)
+                await channel.send(f'Update: {item["content_text"]}')
 
 bot.run(TOKEN)
