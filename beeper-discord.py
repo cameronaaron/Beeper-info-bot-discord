b127ab5a6c7a1890d852914372ffe035f040a659
Cameron Aaron <cameronaaron1@gmail.com>
2024-05-02 23:29:01 -0700

    Initial

---
+++ beeper-discord.py
@@ -0,0 +1,34 @@
+import discord
+from discord.ext import commands, tasks
+import aiohttp
+import json
+
+TOKEN = 'your-discord-bot-token'
+CHANNEL_ID = 123456789
+API_URL_1 = 'https://beeperstatus.com/summary.json'
+API_URL_2 = 'https://beeperstatus.com/v2/components.json'
+
+bot = commands.Bot(command_prefix='!')
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+    check_api.start()  # Start the task when the bot is ready
+
+@tasks.loop(minutes=1)  # Check every 1 minute
+async def check_api():
+    async with aiohttp.ClientSession() as session:
+        async with session.get(API_URL_1) as response1:
+            data1 = await response1.json()
+            if data1['page']['status'] != 'UP':
+                channel = bot.get_channel(CHANNEL_ID)
+                await channel.send(f'There is an outage! Status: {data1["page"]["status"]}')
+
+        async with session.get(API_URL_2) as response2:
+            data2 = await response2.json()
+            for component in data2:
+                if component['status'] != 'OPERATIONAL':
+                    channel = bot.get_channel(CHANNEL_ID)
+                    await channel.send(f'Component {component["name"]} is not operational! Status: {component["status"]}')
+
+bot.run(TOKEN)
